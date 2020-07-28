3da19283337787b8cd541d36196c73daac432dd0
polakv93 <polakpolakiewicz@gmail.com>
2020-07-28 21:24:46 +0200

    init

---
+++ Collection.ConditionalWhere.Tests/QueryTypes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Collection.ConditionalWhere.Tests
+{
+    public class QueryTypes
+    {
+        [Fact]
+        public void Applied_on_simple_string()
+        {
+            var query = CreateQuery();
+            var value = "string";
+
+            var res = query.ConditionalWhere(value, q => q.String == value).ToList();
+
+            res.Should().HaveCount(1);
+            res.First().Id.Should().Be(2);
+        }
+
+        private static IQueryable<QueryItem> CreateQuery()
+        {
+            var query = new List<QueryItem>
+            {
+                new QueryItem { Id = 1 },
+                new QueryItem { Id = 2, Bool = true, String = "string" },
+            }.AsQueryable();
+            return query;
+        }
+    }
+
+    internal class QueryItem    
+    {
+        public int Id { get; set; }
+        public string String { get; set; }
+        public bool Bool { get; set; }
+    }
+}
---
+++ Collection.ConditionalWhere/ConditionalWhereExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Collection.ConditionalWhere
+{
+    public static class ConditionalWhereExtension
+    {
+        public static IQueryable<T> ConditionalWhere<T>(this IQueryable<T> query, string value, Expression<Func<T, bool>> predicate)
+            => !string.IsNullOrWhiteSpace(value) ? query.Where(predicate) : query;
+
+        public static IQueryable<T> ConditionalWhere<T, TK>(this IQueryable<T> query, TK value, Expression<Func<T, bool>> predicate)
+            => EqualityComparer<TK>.Default.Equals(value, default(TK))
+                ? query
+                : query.Where(predicate);
+
+        public static IQueryable<T> ConditionalWhere<T, TK>(this IQueryable<T> query, IEnumerable<TK> value, Expression<Func<T, bool>> predicate)
+            => value != null && value.Any() ? query.Where(predicate) : query;
+    }
+}
